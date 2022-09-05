ccaa47af8558e6e1395929e459db5a9555187d41
kamill <kamillschepuk@gmail.com>
2022-09-05 17:14:11 +0300

    made it better

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client'
 import AppContainer from './components/Table/AppContainer'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/redux-store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
       <Provider store={store}>
         <AppContainer />
       </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
